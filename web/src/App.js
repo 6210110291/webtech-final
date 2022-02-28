8773b872663b03390f54aa89f61e2acc1e602ce3
เมษา เจริญไว <mcharoenwai@gmail.com>
2022-02-28 19:24:48 +0700

    web

--- web/src/App.js
+++ web/src/App.js
@@ -3,12 +3,9 @@ import axios from 'axios';
 const callInfoAPI_read = async () => {
   let result = await axios.get('http://localhost:8080/api/read')
   console.log(result)
-  return <h1>{result}</h1>
 }
 const callInfoAPI_increase = async () => {
-  let result = await axios.post('http://localhost:8080/api/increase', {
-    count: 0
-  })
+  let result = await axios.post('http://localhost:8080/api/increase')
   console.log(result)
 }
 
@@ -17,7 +14,7 @@ function App() {
   return (
     <div>
 
-
+      <h1>{result}</h1>
       <button onClick={callInfoAPI_increase}>increase</button>
 
     </div>
